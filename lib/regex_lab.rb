f92a4aa3abec8763d262917d5b4d395d36bd757a
Joshua Williams <j.o-williams@outlook.com>
2020-04-24 01:43:22 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,10 @@ end
   
 
 def words_five_letters_long(text)
-
+  #returns an array of words that are five letters long
+  return_array = text.split(" ")
+  return_array.grep(/\b\w{5}\b/)
+  #return_array.grep(/\b+\w{4}+\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
